aabce68ae3b04f424a99e3b830f0b0b365d4028f
goraki <gorakio0126@gmail.com>
2021-11-24 23:16:58 +0900

    Add lec-05-07,08

--- Lecture_05_ZMQ_Source_Code/lec-05-prg-08-pub-sub-and-pull-push-client-v2.js
+++ Lecture_05_ZMQ_Source_Code/lec-05-prg-08-pub-sub-and-pull-push-client-v2.js
@@ -1,3 +1,5 @@
+const { time } = require("console");
+
 var zmq = require("zeromq"),
     sock = zmq.socket("sub"),
     pusher = zmq.socket("push");
@@ -7,9 +9,12 @@ pusher.connect("tcp://127.0.0.1:3001");
 sock.subscribe("kitty cats");
 console.log("Subscriber connected to port 3000");
 
-sock.on("message", function(topic, sm) {
+
+clientID = process.argv[2];
+sock.on("message", function (topic, sm) {
+    msg = sm.toString();
     console.log(
-        "I received a message ", sm.toString());
+        `${clientID}: receive status => ${msg}`);
 });
 
 function getRandomInt(min, max) {
@@ -20,8 +25,13 @@ function getRandomInt(min, max) {
 
 setInterval(function () {
     rnum = getRandomInt(1, 100)
-    if (rnum < 10) {
-        console.log("I: sending message ", rnum);
-        pusher.send(rnum);
-    }
+  if (rnum < 10) {
+    console.log(`${clientID}": send status - activated `);
+    msg = "(" + clientID + ":ON)"
+    pusher.send(msg);
+  } else if (rnum > 90) {
+    console.log(`${clientID}": send status - deactivated `);
+    msg = "(" + clientID + ":OFF)"
+    pusher.send(msg);
+  }
 }, 100)
